056c80089b46f1ffebf122bf5a65e689031ba184
JohnathanVu <jonathanvu11@gmail.com>
2025-06-17 11:08:40 -0400

    Cleaned up data

--- dv.py
+++ dv.py
@@ -29,9 +29,7 @@ wait.until(check_departure_button)
 # Pressing enter to submit form works better than clicking button for some reason
 station_name.send_keys(Keys.ENTER)
 
-# TODO
-# Scrape the dv info to get next train info
-# Data to collect: train name, arrival time, train status "All Aboard, In 13 min, etc.", Track #, destination
+# Data to collect: destination, train name, arrival time, train status, track #
 
 # function to check if the dv list exists, because it doesn't initially exist
 def check_departure_vision(driver):
@@ -41,13 +39,32 @@ def check_departure_vision(driver):
 wait = WebDriverWait(driver, 10)
 wait.until(check_departure_vision)
 
+# When the train list exists we can find it, and then find the inner list that contains the data we want
 train_list = driver.find_element(by=By.CSS_SELECTOR, value=".mt-n4")
 trains = train_list.find_elements(by=By.TAG_NAME, value="li")
-for item in trains:
-    print(item.text)
 
-# For debugging: This is for creating an error so the browser doesn't close down
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+# Set used to filter out duplicate info because that happens for some reason
+seen = set()
+data = []
+
+# Scrape data using for loop and item.text and we get all the data we need, but it needs to be cleaned up
+for item in trains:
+    text = item.text.strip()
+    if text and text not in seen:
+
+        seen.add(text)
+
+        # Split into lines and remove empty lines
+        lines = [line.strip() for line in text.split("\n") if line.strip()]
+
+        # Append data into list, remove 3rd element which is always "View Stops"
+        if len(lines) >= 6:
+            station_info = lines[:6]
+            del station_info[2]
+            data.append(station_info)
+print(data)
+
+#TODO
+# Convert data to csv and add to MongoDB
 
 driver.quit()
