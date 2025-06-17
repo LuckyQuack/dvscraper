48bf97c14277ecc9098d5c02c4645eb3ba3c5fab
JohnathanVu <jonathanvu11@gmail.com>
2025-06-17 10:00:38 -0400

    basic webscraping

--- dv.py
+++ dv.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
 
@@ -20,20 +19,34 @@ station_name = driver.find_element(by=By.ID, value="dv-to-station")
 station_name.send_keys("Newark Penn Station")
 
 # function to check if the 'Get Real-Time Departures' Button is clickable because it's disabled until the form gets a valid input
-def element_is_enabled(driver):
+def check_departure_button(driver):
     element = driver.find_element(By.XPATH, "//button[contains(text(), 'Get real-time departures')]")
     return element.is_enabled()
 
 wait = WebDriverWait(driver, 10)
-wait.until(element_is_enabled)
+wait.until(check_departure_button)
 
+# Pressing enter to submit form works better than clicking button for some reason
 station_name.send_keys(Keys.ENTER)
 
 # TODO
 # Scrape the dv info to get next train info
 # Data to collect: train name, arrival time, train status "All Aboard, In 13 min, etc.", Track #, destination
-# More: # of stops
 
+# function to check if the dv list exists, because it doesn't initially exist
+def check_departure_vision(driver):
+    element = driver.find_element(By.CSS_SELECTOR, value=".mt-n4")
+    return element
+
+wait = WebDriverWait(driver, 10)
+wait.until(check_departure_vision)
+
+train_list = driver.find_element(by=By.CSS_SELECTOR, value=".mt-n4")
+trains = train_list.find_elements(by=By.TAG_NAME, value="li")
+for item in trains:
+    print(item.text)
+
+# For debugging: This is for creating an error so the browser doesn't close down
 message = driver.find_element(by=By.ID, value="message")
 text = message.text
 
